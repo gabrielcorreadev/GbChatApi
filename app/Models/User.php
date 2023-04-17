bc13f5351c2edbbc33deb0cbe4e2b0814ffb5407
gabrielcorreadev <30725828+gabrielcorreadev@users.noreply.github.com>
2023-04-17 16:04:24 -0300

    auth 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 use Laravel\Passport\HasApiTokens;
 use App\Notifications\VerifyEmail;
@@ -61,6 +62,30 @@ class User extends Authenticatable implements MustVerifyEmail
     public function sendEmailVerificationNotification()
     {
         $this->notify(new VerifyEmail); // my notification
+    }
+
+    public function generateCode()
+    {
+        $code = rand(1000, 9999);
+
+        UserCode::updateOrCreate(
+            [ 'user_id' => auth()->id() ],
+            [ 'code' => $code ]
+        );
+
+        try {
+
+            $details = [
+                'title' => 'Mail from shouts.dev',
+                'code' => $code
+            ];
+
+            Mail::to(auth()->user()->email)->send(new SendCodeMail($details));
+
+        } catch (\Exception $e) {
+            info("Error: ". $e->getMessage());
+            dd($e);
+        }
     }
 
     public function getPhotoAttribute()
